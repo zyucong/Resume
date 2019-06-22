ba3d794c7d075e26b4cbb9770ed67475a641c8ea
Yingcong Zhu From Mac <yingcongzhu@gmail.com>
2019-06-22 22:46:36 +1000

    first commit, a skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Card v-for="(card, index) in cards" :key="index" :plain="card.plain" :color="card.color" :card-id="card.id">
+      <Glider v-if="component == 'title'" name="Julian Zhu" />
+    </Card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
+import Glider from '@/components/Glider.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      cards: [
+        {id: 1, plain: true, color: '#272B30', component: "title"},
+        {id: 2, plain: false, color: null}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card-container">
+      <div v-if="plain" class="poster plain-bg">
+        <div class="placeholder">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Card",
+    props: ["card-id", "color", "plain", "component"]
+}
+</script>
+
+<style scoped>
+.plain-bg {
+    background-color: #272B30;
+}
+.poster {
+    width: 100vw;
+    height: 100vh;
+}
+
+.placeholder {
+    width: 100%;
+    height: 100%;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
